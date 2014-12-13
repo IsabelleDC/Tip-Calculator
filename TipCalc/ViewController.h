eb42954198247e33c789ef023f3da057571470de
IsabelleRey <isabellrey@gmail.com>
2014-12-13 10:48:47 -0800

    Initial Commit

---
+++ TipCalc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalc
+//
+//  Created by Zai on 12/13/14.
+//  Copyright (c) 2014 Isa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
